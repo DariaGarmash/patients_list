bd1dab26189673b39ed10362984ab1fd9a88fd06
DariaGarmash <daria.garmash@gmail.com>
2023-11-07 12:26:27 +0100

    cleanup

--- src/tests/adapter/patientsDataAdapter.test.tsx
+++ src/tests/adapter/patientsDataAdapter.test.tsx
@@ -1,20 +1,5 @@
 import { PatientEntity, TPatientSex, TVaccinationStatus, VaccinationStatusDetector } from "../../adapters/patientsDataAdapter";
 
-
-const testStatusCasesFemale: Array<[Partial<PatientEntity>, TVaccinationStatus]> = [
-    [{ sex: 'female', age: 6, isVaccinated: false, vaccinationDate: null }, 'not_done'],
-    [{ sex: 'female', age: 8, isVaccinated: false, vaccinationDate: null }, 'ready_to_schedule'],
-    [{ sex: 'female', age: 10, isVaccinated: false, vaccinationDate: null }, 'overdue'],
-    [{ sex: 'female', age: 14, isVaccinated: true, vaccinationDate: '2023-01-01' }, 'done'],
-];
-
-const testStatusCasesMale: Array<[Partial<PatientEntity>, TVaccinationStatus]> = [
-    [{ sex: 'male', age: 6, isVaccinated: false, vaccinationDate: null }, 'not_done'],
-    [{ sex: 'male', age: 8, isVaccinated: false, vaccinationDate: null }, 'ready_to_schedule'],
-    [{ sex: 'male', age: 10, isVaccinated: false, vaccinationDate: null }, 'overdue'],
-    [{ sex: 'male', age: 14, isVaccinated: true, vaccinationDate: '2023-01-01' }, 'done'],
-];
-
 const verifyTestCases = (detector: VaccinationStatusDetector, testCases: Array<[Partial<PatientEntity>, TVaccinationStatus]>) => {
     testCases.forEach(([patientData, expectedStatus]) => {
         const status = detector.detectStatus({
