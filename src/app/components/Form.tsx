e9b4c2f29a392f4f85dbf4ec4b77ab5022d63585
DariaGarmash <daria.garmash@gmail.com>
2023-11-08 10:30:22 +0100

    test for form

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,4 +1,4 @@
-import React, {ComponentPropsWithoutRef, useState, ChangeEvent, FormEventHandler} from "react";
+import React, { ComponentPropsWithoutRef, useState, ChangeEvent, FormEventHandler } from "react";
 import Button from "./Button";
 
 export type FormInput = ComponentPropsWithoutRef<'input'> & {
@@ -16,13 +16,13 @@ export type FormProps<T extends Record<string, any>> = {
     initialFormData: T,
     errorMessage?: string,
 }
-  
+
 const Form = <T extends Record<string, any>>({
     submit, inputs, initialFormData, errorMessage = ''
 }: FormProps<T>): JSX.Element => {
 
     const [formData, setFormData] = useState<T>(initialFormData)
-   
+
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
         setFormData(prev => ({
             ...prev,
@@ -43,7 +43,7 @@ const Form = <T extends Record<string, any>>({
                     {inputs.map((input: FormInput) => (
                         <div className="form-control" key={input.name}>
                             <label htmlFor="email">{input.label}</label>
-                            <input type={input.type} name={input.name} required={input.required}
+                            <input type={input.type} name={input.name} required={input.required} data-testid={`input-${input.name}`}
                                 onChange={onChange} />
                         </div>
                     ))}
